7b3a25db8cf48203e1e00da4f36796d9b0bd38b0
Tim <code@tim.life>
2025-12-02 14:33:59 -0800

    🔼⚙️ [skip ci] auto-commit

--- tests/process/test_lifecycle_comprehensive_coverage_managedprocessedgecases_waitforprocessoutput_etc.py
+++ tests/process/test_lifecycle_comprehensive_coverage_managedprocessedgecases_waitforprocessoutput_etc.py
@@ -75,7 +75,8 @@ class TestWaitForProcessOutput(FoundationTestCase):
             [
                 sys.executable,
                 "-c",
-                "import sys; print('start|middle|end', flush=True)",
+                # Print output then sleep to keep process alive for monitoring
+                "import sys, time; print('start|middle|end', flush=True); time.sleep(2)",
             ],
             capture_output=True,
             text_mode=True,
